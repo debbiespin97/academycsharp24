3d0370f41c20d56f7982bb52245d2b049b5cd7e8
debys <debys@LENOVODEBH>
2024-10-15 15:59:36 +0200

    Aggiungere i file di progetto.

---
+++ ConsoleAppacademy/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+int a = 0;
+
+#region Codice di esempio
+if (a == 0)
+#endregion
+
+#if DEBUG
+    for (int i = 1; i <= 10; i++)
+    {
+        Console.WriteLine("Siamo in debug!");
+        Console.WriteLine(i);
+    }
+#endif
+
+#if RELEASE
+   for (int i = 1; i <= 20; i++)
+    {
+        Console.WriteLine("Siamo in Release!");
+        Console.WriteLine(i);
+    }
+#endif
+
+
